264bf5be279424ce977973446e2051c142b923be
Kopi <it17142038@my.sliit.lk>
2024-02-05 14:06:59 +0530

    feat: Update unit tests for pages

--- pages/tests.py
+++ pages/tests.py
@@ -1,18 +1,19 @@
-from django.test import TestCase
 from django.test import SimpleTestCase
-from django.urls import reverse
+from django.urls import reverse, resolve
+
+from .views import HomePageView
 
 
 class HomepageTests(SimpleTestCase):
+    def setUp(self):
+        url = reverse("home")
+        self.response = self.client.get(url)
+
     def test_url_exists_at_correct_location(self):
         response = self.client.get("/")
         self.assertEqual(response.status_code, 200)
 
-    def test_homepage_url_name(self):
-        response = self.client.get(reverse("home"))
-        self.assertEqual(response.status_code, 200)
-
-    def test_homepage_template(self):  # new
+    def test_homepage_template(self):
         response = self.client.get("/")
         self.assertTemplateUsed(response, "pages/home.html")
 
@@ -23,3 +24,7 @@ class HomepageTests(SimpleTestCase):
     def test_homepage_does_not_contain_incorrect_html(self):
         response = self.client.get("/")
         self.assertNotContains(response, "Hi there! I should not be on the page.")
+
+    def test_homepage_url_resolves_homepageview(self):
+        view = resolve("/")
+        self.assertEqual(view.func.__name__, HomePageView.as_view().__name__)
