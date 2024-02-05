17d708085ed4c93cc97bb6b493fdf4784e4f2995
Kopi <it17142038@my.sliit.lk>
2024-02-05 12:27:26 +0530

    feat: Migrate to PostgreSQL

--- django_project/settings.py
+++ django_project/settings.py
@@ -77,8 +77,12 @@ TEMPLATES = [
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "db",  # set in docker-compose.yml
+        "PORT": 5432,  # default postgres port
     }
 }
 
